0ac6c8a15e9bc1ae97a2ff9821a6f44dbaf9b589
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-04 22:13:30 +0530

    commit

---
+++ src/assignmentone/utilities/EmployeeUtilities.java
@@ -0,0 +1,29 @@
+package assignmentone.utilities;
+
+
+
+/**
+ * Provides utility methods for Employee objects.
+ */
+public class EmployeeUtilities {
+
+    /**
+     * Prints the details of an employee.
+     * Demonstrates usage of public getters.
+     */
+    public static void printEmployeeDetails(assignmentone.employees.Employee emp) {
+        System.out.println("Employee Name: " + emp.getName());
+        System.out.println("Employee ID: " + emp.getEmployeeId());
+        System.out.println("Salary: " + emp.getSalary());
+    }
+
+    /**
+     * Gives a raise to an employee by percentage.
+     * Demonstrates modification through public setters.
+     */
+    public static void giveRaise(assignmentone.employees.Employee emp, double percent) {
+        double newSalary = emp.getSalary() + (emp.getSalary() * percent / 100);
+        emp.setSalary(newSalary);
+        System.out.println(emp.getName() + " has received a " + percent + "% raise. New Salary: " + emp.getSalary());
+    }
+}
