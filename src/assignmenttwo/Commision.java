bf153bf6f21ce393c686b9bf1b9e6b22e3d5e866
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-04 22:17:47 +0530

    commit

---
+++ src/assignmenttwo/Commision.java
@@ -0,0 +1,52 @@
+package assignmenttwo;
+import java.util.Scanner;
+
+public class Commision {
+
+	private String name;
+    private String address;
+    private String phone;
+    private double salesAmount;
+
+    /**
+     * Accepts details of the sales employee.
+     */
+    public void acceptDetails() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Address: ");
+        address = sc.nextLine();
+
+        System.out.print("Enter Phone: ");
+        phone = sc.nextLine();
+
+        System.out.print("Enter Sales Amount: ");
+        salesAmount = sc.nextDouble();
+    }
+
+    /**
+     * Calculates commission based on sales amount.
+     */
+    public void calculateCommission() {
+        double commission = 0;
+
+        if (salesAmount >= 100000) {
+            commission = salesAmount * 0.10;
+        } else if (salesAmount >= 50000) {
+            commission = salesAmount * 0.05;
+        } else if (salesAmount >= 30000) {
+            commission = salesAmount * 0.03;
+        } // else commission stays 0
+
+        System.out.println("\n--- Employee Commission Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Phone: " + phone);
+        System.out.println("Sales Amount: " + salesAmount);
+        System.out.println("Commission: " + commission);
+    }
+
+}
